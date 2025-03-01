e0b530cfa32aa9608eec0b5c804e13f82067def9
Jakku Night <R101g117l117s@gmail.com>
2025-03-01 20:39:55 -0300

    Rebuilding the theme.

--- flake.nix
+++ flake.nix
@@ -4,50 +4,16 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs";
     };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-    genshin-theme = {
-      url = "github:nicefaa6waa/genshin-sddm-theme";
-      flake = false;
-    };
-    nightbg = {
-      url = "file+https://www.dropbox.com/scl/fi/e6kio0zet8j5lax9ztu7y/nightbg.mp4?rlkey=29ni1jq0a9sr02b6l0s61oiiq&st=en7rqgmy&dl=0";
-      flake = false;
-    };
-    morningbg = {
-      url = "file+https://www.dropbox.com/scl/fi/8om5p5d3ul984krbnhfgu/morningbg.mp4?rlkey=jedf5qcycj310oqx8djy9yomz&st=yupg7zz8&dl=0";
-      flake = false;
-    };
-    sunrisebg = {
-      url = "file+https://www.dropbox.com/scl/fi/f3x5jfr8sk06ij3c8kgoy/sunrisebg.mp4?rlkey=r0lzylvhze4lq3xglwkvf4iya&st=ybukfsgv&dl=0";
-      flake = false;
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, ... } @ inputs:
   let
+    system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    packages.default = pkgs.stdenv.mkDerivation {
-      name = "genshin-sddm-theme";
-      src = inputs.genshin-theme;
-      dontWrapQtApps = false;
-      nativeBuildInputs = with pkgs.libsForQt5.qt5; [
-        qtdeclarative
-        wrapQtAppsHook
-        qtmultimedia
-        qtquickcontrols
-        qtgraphicaleffects
-      ];
-      dontBuild = true;
-      installPhase = ''
-        mkdir -p $out
-        cp -R ./* $out/
-        cp ${inputs.nightbg} $out/backgrounds
-        cp ${inputs.morningbg} $out/backgrounds
-        cp ${inputs.sunrisebg} $out/backgrounds
-      '';
+    packages.${system} = {
+      genshin-login-theme = import ./genshin-login-theme;
+      default = self.outputs.packages.${system}.genshin-login-theme;
     };
-  });
+  };
 }
---
+++ genshin-login-theme/Main.qml
@@ -0,0 +1,10 @@
+// Imports:
+import QtQuick
+
+Rectangle {
+  width: 1920
+  height: 1080
+  color: "black"
+
+  
+}
